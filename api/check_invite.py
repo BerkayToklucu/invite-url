d4b7c369ef311951b04ecf11965c6de505906cce
A. Berkay Toklucu <retroanonim@gmail.com>
2024-08-08 16:43:57 +0300

    Update check_invite.py

--- api/check_invite.py
+++ api/check_invite.py
@@ -9,24 +9,28 @@ BOT_TOKEN = os.getenv('BOT_TOKEN')
 
 @app.route('/api/check_invite', methods=['POST'])
 def check_invite_availability():
-    keyword = request.form.get('keyword')
-    
-    if not keyword:
-        return jsonify({'result': 'Geçersiz anahtar kelime.'}), 400
+    try:
+        data = request.get_json()
+        keyword = data.get('keyword')
+        
+        if not keyword:
+            return jsonify({'result': 'Geçersiz anahtar kelime.'}), 400
 
-    url = f"https://discord.com/api/v10/invites/{keyword}"
-    headers = {
-        "Authorization": f"Bot {BOT_TOKEN}"
-    }
+        url = f"https://discord.com/api/v10/invites/{keyword}"
+        headers = {
+            "Authorization": f"Bot {BOT_TOKEN}"
+        }
 
-    response = requests.get(url, headers=headers)
+        response = requests.get(url, headers=headers)
 
-    if response.status_code == 404:
-        return jsonify({'result': f"https://discord.gg/{keyword} kullanılabilir!"})
-    elif response.status_code == 200:
-        return jsonify({'result': f"https://discord.gg/{keyword} zaten başka bir sunucu tarafından kullanılıyor."})
-    else:
-        return jsonify({'result': f"Bir hata oluştu: {response.status_code} - {response.text}"})
+        if response.status_code == 404:
+            return jsonify({'result': f"https://discord.gg/{keyword} kullanılabilir!"})
+        elif response.status_code == 200:
+            return jsonify({'result': f"https://discord.gg/{keyword} zaten başka bir sunucu tarafından kullanılıyor."})
+        else:
+            return jsonify({'result': f"Bir hata oluştu: {response.status_code} - {response.text}"})
+    except Exception as e:
+        return jsonify({'result': f"Bir hata oluştu: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
