31ce6c47b849a10a179c92af72d38aa38302d791
BerkayToklucu <retroanonim@gmail.com>
2024-08-08 23:53:04 +0300

    update js

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ function checkURL() {
         const output = document.getElementById("output");
         const result = document.createElement("p");
         result.textContent = data.result;
+
+        // Renklendirme için değişiklikler
+        if (data.result.includes("zaten başka bir sunucu tarafından kullanılıyor. Alternatif:")) {
+            result.style.color = "yellow"; // Alternatif önerisi
+        } else if (data.result.includes("zaten başka bir sunucu tarafından kullanılıyor, ancak uygun bir alternatif bulunamadı.")) {
+            result.style.color = "red"; // Sunucu adı kullanılıyor, alternatif yok
+        } else if (data.result.includes("kullanılabilir.")) {
+            result.style.color = "green"; // Kullanılabilir
+        }
+
         output.appendChild(result);
     })
     .catch(error => console.error('Error:', error));
